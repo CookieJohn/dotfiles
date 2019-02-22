29b86cca113e248bb2bd71487fa08121b06c5d8d
John <johnwu2613@gmail.com>
2019-02-22 08:55:35 +0800

    Add collect and update sh file

---
+++ collect.sh
@@ -0,0 +1,6 @@
+cp ~/.zshrc .zshrc && echo collect zshrc OK || echo collect zshrc Failed
+cp ~/.vimrc .vimrc && echo collect vimrc OK || echo collect vimrc Failed
+cp ~/.vimrc.local .vimrc.local && echo collect vimrc.local OK || echo collect vimrc.local Failed
+cp ~/.vimrc.bundles.local .vimrc.bundles.local && echo collect vimrc.bundles.local OK || echo collect vimrc.bundles.local Failed
+cp ~/.tmux.conf .tmux.conf && echo collect tmux.conf OK || echo collect tmux.conf Failed
+cp ~/.pryrc .pryrc && echo collect pryrc OK || echo collect pryrc Failed
---
+++ update.sh
@@ -0,0 +1,6 @@
+cp .zshrc ~/.zshrc && echo update zshrc OK || echo update zshrc Failed
+cp .vimrc ~/.vimrc && echo update vimrc OK || echo update vimrc Failed
+cp .vimrc.local ~/.vimrc.local && echo update vimrc.local OK || echo update vimrc.local Failed
+cp .vimrc.bundles.local ~/.vimrc.bundles.local && echo update vimrc.bundles.local OK || echo update vimrc.bundles.local Failed
+cp .tmux.conf ~/.tmux.conf && echo update tmux.conf OK || echo update tmux.conf Failed
+cp .pryrc ~/.pryrc && echo update pryrc OK || echo update pryrc Failed
