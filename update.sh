7b0d9fe4a55f1378cdd000542780866f1a7c8303
John <johnwu2613@gmail.com>
2019-03-18 09:37:25 +0800

    Add default source zshrc

--- update.sh
+++ update.sh
@@ -4,3 +4,4 @@ cp .vimrc.local ~/.vimrc.local && echo update vimrc.local OK || echo update vimr
 cp .vimrc.bundles.local ~/.vimrc.bundles.local && echo update vimrc.bundles.local OK || echo update vimrc.bundles.local Failed
 cp .tmux.conf ~/.tmux.conf && echo update tmux.conf OK || echo update tmux.conf Failed
 cp .pryrc ~/.pryrc && echo update pryrc OK || echo update pryrc Failed
+source ~/.zshrc && echo source zshrc done!
